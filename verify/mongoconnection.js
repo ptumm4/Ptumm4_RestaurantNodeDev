d08b7067cabdd5899341d657603ba3b1a90a2ee4
Munna <munna@Munnas-MacBook-Pro.local>
2023-12-06 00:40:33 -0500

    first commit

---
+++ verify/mongoconnection.js
@@ -0,0 +1,6 @@
+module.exports = {
+    connectionurl: 'mongodb+srv://prathimatummala123:Prathima1%40@ptumm4.fsmllsj.mongodb.net/',
+    database: 'restaurantdatabase',
+    collection: 'restaurantcollection'
+  };
+  
